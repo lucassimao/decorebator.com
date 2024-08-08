e42aa9b023e3d8d27f1503e272b7839a84271b7b
Lucas Simão <lsimaocosta@gmail.com>
2024-08-08 11:45:02 -0300

    wip

---
+++ mobile/api/constants.ts
@@ -0,0 +1,3 @@
+export const AUTH_REQUIRED_ERROR = "Authentication required.";
+export const DEFAULT_ERROR = "Could not process your request.";
+export const TOKEN_VALIDATION_ERROR = "Token vaidation error";
