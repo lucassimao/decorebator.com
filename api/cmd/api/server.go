08d7566f26dfdb4973cc61dc0aa856543d5ae90d
Lucas Simão <lsimaocosta@gmail.com>
2025-04-13 16:27:51 -0300

    moved backend to api folder

---
+++ api/cmd/api/server.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"decorebator.com/internal/common"
+	decorebator "decorebator.com/internal/http"
+	"decorebator.com/internal/workers"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/dig"
+)
+
+func main() {
+
+	if os.Getenv("ENV") == "production" {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	// Setup dependency injection
+	var container = dig.New()
+	container.Provide(workers.NewContentGenerationService)
+
+	var srv = &http.Server{
+		Addr:    ":" + os.Getenv("PORT"),
+		Handler: decorebator.SetupRoutes(container),
+	}
+
+	// Run server in a goroutine so that it doesn't block
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+	// kill (no param) default send syscanll.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall. SIGKILL but can"t be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	if os.Getenv("ENV") == "production" {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		// catching ctx.Done(). timeout of 5 seconds.
+		select {
+		case <-ctx.Done():
+			log.Println("timeout of 5 seconds.")
+		}
+	} else {
+		if err := srv.Shutdown(context.Background()); err != nil {
+			log.Fatal("Server Shutdown:", err)
+		}
+	}
+
+	log.Println("Server exiting")
+	defer common.CloseDBConnection()
+}
